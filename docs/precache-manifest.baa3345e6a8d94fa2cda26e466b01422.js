cd5e4fb5d7ff7c9f26504d56b69540e1d16140cc
Sava <s86.kovacevic@gmail.com>
2020-03-12 01:08:24 +0100

    1

---
+++ docs/precache-manifest.baa3345e6a8d94fa2cda26e466b01422.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edc588362c3476dc3b57b8468ef2098e",
+    "url": "/una-clinic/index.html"
+  },
+  {
+    "revision": "cca906a88991149b9776",
+    "url": "/una-clinic/static/css/main.5957d86f.chunk.css"
+  },
+  {
+    "revision": "7b5907df2ea47e17e524",
+    "url": "/una-clinic/static/js/2.34aeb33c.chunk.js"
+  },
+  {
+    "revision": "cca906a88991149b9776",
+    "url": "/una-clinic/static/js/main.d89cf1e0.chunk.js"
+  },
+  {
+    "revision": "6371a603d5c38118bfb0",
+    "url": "/una-clinic/static/js/runtime-main.afb76636.js"
+  },
+  {
+    "revision": "0cae0127febafa5386ff681642350c7a",
+    "url": "/una-clinic/static/media/1.0cae0127.jpeg"
+  },
+  {
+    "revision": "60a17e10ce6f9b27441c97e0290d6608",
+    "url": "/una-clinic/static/media/1.60a17e10.jpg"
+  },
+  {
+    "revision": "4e08dd1037b2d6ae3b3286f12c16010b",
+    "url": "/una-clinic/static/media/2.4e08dd10.jpeg"
+  },
+  {
+    "revision": "d97adbb302b9812d5e5d342e36c54214",
+    "url": "/una-clinic/static/media/2.d97adbb3.jpg"
+  },
+  {
+    "revision": "6b7ed47938e9fc80dbf48bb78f44a1d5",
+    "url": "/una-clinic/static/media/3.6b7ed479.jpeg"
+  },
+  {
+    "revision": "d901472e08ef4186eec1fd4c265b251d",
+    "url": "/una-clinic/static/media/3.d901472e.jpg"
+  },
+  {
+    "revision": "efb3b92b23df2888861aad2e98814c73",
+    "url": "/una-clinic/static/media/4.efb3b92b.jpg"
+  },
+  {
+    "revision": "2c47fd7f8b926768f7626fde2d856e8d",
+    "url": "/una-clinic/static/media/5.2c47fd7f.jpg"
+  },
+  {
+    "revision": "a0619916bbefc11acbf3a61c185f8bb0",
+    "url": "/una-clinic/static/media/6.a0619916.jpg"
+  },
+  {
+    "revision": "a076171d0df46e75ac1955f5ebab7270",
+    "url": "/una-clinic/static/media/7.a076171d.jpg"
+  },
+  {
+    "revision": "939195e5ba2ebf78a3e49752afe4a2ce",
+    "url": "/una-clinic/static/media/8.939195e5.jpg"
+  },
+  {
+    "revision": "137199eda88073fa07b8ed9b3b25f297",
+    "url": "/una-clinic/static/media/Montserrat-Bold.137199ed.eot"
+  },
+  {
+    "revision": "4106bfb4e9caf2df264a4ae2f0d2ea8b",
+    "url": "/una-clinic/static/media/Montserrat-Bold.4106bfb4.ttf"
+  },
+  {
+    "revision": "5e0f4f019022f528c3ab7c86fbb815e5",
+    "url": "/una-clinic/static/media/Montserrat-Bold.5e0f4f01.svg"
+  },
+  {
+    "revision": "8a651e1b8b7c465bdbb7dfaef8edbec1",
+    "url": "/una-clinic/static/media/Montserrat-Bold.8a651e1b.woff2"
+  },
+  {
+    "revision": "8beb96f2a9c0e74b9da2982eaeafe89c",
+    "url": "/una-clinic/static/media/Montserrat-Bold.8beb96f2.woff"
+  },
+  {
+    "revision": "1661acf2e182c2464f280aa57dfad437",
+    "url": "/una-clinic/static/media/Montserrat-Light.1661acf2.svg"
+  },
+  {
+    "revision": "2fb95d6b3ec5d2b55bb23c7b9ebdccfe",
+    "url": "/una-clinic/static/media/Montserrat-Light.2fb95d6b.woff2"
+  },
+  {
+    "revision": "76ff679837e0ad31f072e419c129d920",
+    "url": "/una-clinic/static/media/Montserrat-Light.76ff6798.eot"
+  },
+  {
+    "revision": "bd84ea9c2043e221cd33f2a06a2643b9",
+    "url": "/una-clinic/static/media/Montserrat-Light.bd84ea9c.woff"
+  },
+  {
+    "revision": "d1e0dbba19aa3f0eacf0446127ba43bb",
+    "url": "/una-clinic/static/media/Montserrat-Light.d1e0dbba.ttf"
+  },
+  {
+    "revision": "7c974c43b442779ccee2c0e5e5c4ffe5",
+    "url": "/una-clinic/static/media/Montserrat-Regular.7c974c43.woff2"
+  },
+  {
+    "revision": "b02836612f1af63df5c2a537dbfd5c38",
+    "url": "/una-clinic/static/media/Montserrat-Regular.b0283661.svg"
+  },
+  {
+    "revision": "ba2839bb6c710198330d025b7ae798e4",
+    "url": "/una-clinic/static/media/Montserrat-Regular.ba2839bb.eot"
+  },
+  {
+    "revision": "da3a14142000cd8c56d62f0aa009e3fa",
+    "url": "/una-clinic/static/media/Montserrat-Regular.da3a1414.ttf"
+  },
+  {
+    "revision": "dd15dd60a7b5791a1b742a83ec64d2d1",
+    "url": "/una-clinic/static/media/Montserrat-Regular.dd15dd60.woff"
+  },
+  {
+    "revision": "b0d0e2b194ac36a1dfa69f872d855bfa",
+    "url": "/una-clinic/static/media/ana.b0d0e2b1.jpg"
+  },
+  {
+    "revision": "876ac07af07f97f4e866b9d1380a3a84",
+    "url": "/una-clinic/static/media/baby.876ac07a.svg"
+  },
+  {
+    "revision": "23f4a12c9793bb26061056e4d55f5c0c",
+    "url": "/una-clinic/static/media/blogpost.23f4a12c.jpg"
+  },
+  {
+    "revision": "8551b010a986ae77e57e66073a8cd261",
+    "url": "/una-clinic/static/media/call.8551b010.svg"
+  },
+  {
+    "revision": "4772bc6ad59bd534d7367e277a231f98",
+    "url": "/una-clinic/static/media/derma.4772bc6a.svg"
+  },
+  {
+    "revision": "049a79f8d601c0191edd32c34d3bfc87",
+    "url": "/una-clinic/static/media/eye.049a79f8.svg"
+  },
+  {
+    "revision": "cc8130bd100f4d6e7aca58b151ae1ef4",
+    "url": "/una-clinic/static/media/infect.cc8130bd.svg"
+  },
+  {
+    "revision": "b60b50b037274b0b0d25b1a920eb6d03",
+    "url": "/una-clinic/static/media/logo.b60b50b0.png"
+  },
+  {
+    "revision": "d133554be19e9fcb9eed5bf2cae5bc79",
+    "url": "/una-clinic/static/media/maljkovic.d133554b.jpg"
+  },
+  {
+    "revision": "d55e155dc2917a47c9be4a9adcf1c7c7",
+    "url": "/una-clinic/static/media/mental.d55e155d.svg"
+  },
+  {
+    "revision": "1debcf742ade913ab019fafd81471c37",
+    "url": "/una-clinic/static/media/psy.1debcf74.svg"
+  },
+  {
+    "revision": "287215a90859e2b6e8b34dbc7a74fe93",
+    "url": "/una-clinic/static/media/radio.287215a9.svg"
+  },
+  {
+    "revision": "c28b7ced2e823669fc639cb80a59eb38",
+    "url": "/una-clinic/static/media/rehab.c28b7ced.svg"
+  },
+  {
+    "revision": "6dfe41eeaf8017e504aaea74a7dd764e",
+    "url": "/una-clinic/static/media/scalpel.6dfe41ee.svg"
+  },
+  {
+    "revision": "b6abd903590766c181551401bbec0428",
+    "url": "/una-clinic/static/media/search.b6abd903.svg"
+  },
+  {
+    "revision": "e9ea69d75567276a4485ec8beb1a6729",
+    "url": "/una-clinic/static/media/stethoscope.e9ea69d7.svg"
+  },
+  {
+    "revision": "84260601edf21f7dec9990cf1cc8a21d",
+    "url": "/una-clinic/static/media/uterus.84260601.svg"
+  }
+]);
